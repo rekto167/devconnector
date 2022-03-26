18c50392e4d3f4333df52239a38b2a2c24df7c75
mirebus <ekysr99@gmail.com>
2022-03-27 05:01:35 +0700

    Connecting to mongodb

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./config/db');
+
+// connecting to database
+connectDB();
 
 app.get('/', (req, res) => {
     res.send('API running');
