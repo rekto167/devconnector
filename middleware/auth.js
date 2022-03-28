cb669277ba7b8ce3a136914058a2a461796c0b3e
mirebus <ekysr99@gmail.com>
2022-03-28 10:12:28 +0700

    costum auth middleware & auth verify

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+const config = require('config')
+
+module.exports = function(req, res, next) {
+    const token = req.header('Authorization');
+
+    if(!token){
+        return res.status(401).json({msg: 'No token, authorization failed'});
+    }
+
+    try{
+        const decoded = jwt.verify(token, config.get('jwtToken'))
+
+        req.user = decoded.user;
+        next();
+    }catch(err){
+        res.status(401).json({msg:'Token is not valid'})
+    }
+}
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
 // @router  GET api/auth
 // @desc    Test Route
 // @access  Public
-router.get('/', (req, res) => {
+router.get('/', auth, (req, res) => {
     res.send('Auth route');
 })
 
