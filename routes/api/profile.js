0a0daa15e5a327ab0d722951e6f53be714157211
mirebus <ekysr99@gmail.com>
2022-03-29 13:36:01 +0700

    add & delete profile education

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -178,4 +178,58 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
     }
 })
 
+// @router  PUT api/profile/education
+// @desc    Put education profile
+// @access  Private
+router.put('/education', [auth, [
+    check('school', 'school is required').not().isEmpty(),
+    check('degree', 'degree is required').not().isEmpty(),
+    check('fieldofstudy', 'field of study is required').not().isEmpty(),
+    check('from', 'From date is required').not().isEmpty(),
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const{school, degree, fieldofstudy, location, from, to, current, description} = req.body;
+
+    const newEdu = {school, degree, fieldofstudy, location, from, to, current, description}
+
+    try {
+        let profile = await Profile.findOne({user: req.user.id});
+        if(!profile){
+            return res.status(400).json({msg:'There is no profile for this user.'})
+        }
+        profile.education.unshift(newEdu);
+
+        await profile.save();
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+
+})
+
+// @router  DELETE api/profile/education/:edu_id
+// @desc    Delete profile education
+// @access  Private
+router.delete('/education/:edu_id', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({user: req.user.id});
+
+        const removeIndex = profile.education.map(item => item.id).indexOf(req.params.edu_id);
+
+        profile.education.splice(removeIndex, 1);
+
+        await profile.save()
+
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+})
+
 module.exports = router;
