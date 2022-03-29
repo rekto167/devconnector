de269cfc76ff6522dbf1038d04580e2102e71437
mirebus <ekysr99@gmail.com>
2022-03-29 11:12:58 +0700

    add profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -125,4 +125,37 @@ router.delete('/', auth, async (req, res) => {
     }
 })
 
+// @router  PUT api/profile/experience
+// @desc    Put experience profile
+// @access  Private
+router.put('/experience', [auth, [
+    check('title', 'Title is required').not().isEmpty(),
+    check('company', 'Company is required').not().isEmpty(),
+    check('from', 'From date is required').not().isEmpty(),
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const{title, company, location, from, to, current, description} = req.body;
+
+    const newExp = {title, company, location, from, to, current, description}
+
+    try {
+        let profile = await Profile.findOne({user: req.user.id});
+        if(!profile){
+            return res.status(400).json({msg:'There is no profile for this user.'})
+        }
+        profile.experience.unshift(newExp);
+
+        await profile.save();
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+
+})
+
 module.exports = router;
