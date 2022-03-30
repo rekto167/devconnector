dc34ec746dd9c5e7024e2c31e28dd85d9a0cc4d9
mirebus <ekysr99@gmail.com>
2022-03-30 08:27:38 +0700

    add comment & delete comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -122,6 +122,64 @@ router.put('/unlike/:id_post', auth, async (req, res) => {
     }
 })
 
+// @router  PUT api/posts/comment/:id_post
+// @desc    Add comment
+// @access  Private
+router.put('/comment/:id_post', [auth,
+    [
+        check('text', 'Text is required').not().isEmpty()
+    ]
+], async(req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(404).json({errors: errors.array()});
+    }
 
+    try {
+        let post = await Post.findById(req.params.id_post);
+
+        const user = await User.findById(req.user.id).select('-password');
+
+        const newComment = {
+            user: user.id,
+            text: req.body.text,
+            avatar: user.avatar,
+        }
+
+        post.comments.unshift(newComment);
+
+        await post.save();
+
+        res.json(post.comments);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+})
+
+// @router  DELETE api/posts/comment/:id_post/:id_comment
+// @desc    Delete comment
+// @access  Private
+router.delete('/comment/:id_post/:id_comment', auth, async (req, res) => {
+    let post = await Post.findById(req.params.id_post);
+
+    const comment = post.comments.find(comment => comment.id === req.params.id_comment);
+
+    if(!comment) {
+        return res.status(404).json({msg: 'Comment does not exist'});
+    }
+
+    if(comment.user.toString() !== req.user.id){
+        return res.status(401).json({msg: 'Not authorized for this comment'});
+    }
+
+    const removeIndex = post.comments.map( comment => comment.user.toString()).indexOf(req.user.id);
+
+    post.comments.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json({msg: 'Comment deleted'});
+})
 
 module.exports = router;
