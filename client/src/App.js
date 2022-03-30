2741765084356106fd377b63238746120aef34ea
mirebus <ekysr99@gmail.com>
2022-03-30 13:48:03 +0700

    React router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import React, {Fragment} from 'react'
+import {BrowserRouter as Router, Route, Routes, Switch} from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
 import './App.css'
 
 const App = () => {
   return (
-    <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+        </Routes>
+      </Fragment>
+    </Router>
   )
 }
 
