7a3fa257cd170b6c8dd17fa85200118d8bc9a011
mirebus <ekysr99@gmail.com>
2022-04-03 04:02:47 +0700

    Protected route for dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,9 @@ import Landing from './components/layout/Landing'
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
 import Alert from './components/layout/Alert'
+import Dashboard from './components/Dashboard'
+
+import PrivateRoute from './routing/PrivateRoute'
 
 import {Provider} from 'react-redux';
 import store from './store';
@@ -34,6 +37,10 @@ const App = () => {
               <Route exact path="/" element={<Landing />} />
               <Route exact path="/login" element={<Login />} />
               <Route exact path="/register" element={<Register />} />
+              <Route
+                path="/dashboard"
+                element={<PrivateRoute component={Dashboard} />}
+              />
           </Routes>
         </Fragment>
       </Router>
