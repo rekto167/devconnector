b3e3676af87f17733f537c01347633b79ce284d9
mirebus <ekysr99@gmail.com>
2022-04-03 13:44:54 +0700

    Starting on dashboard

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,4 +1,4 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT } from "./types";
+import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT, CLEAR_PROFILE } from "./types";
 import axios from 'axios'
 import { setAlert } from "./alert";
 import setAuthToken from "../utils/setAuthToken";
@@ -84,5 +84,6 @@ export const login = ({email, password}) => async dispatch => {
 
 // logout
 export const logout = () => dispatch => {
+    dispatch({type: CLEAR_PROFILE})
     dispatch({type: LOGOUT})
 }
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,15 +1,37 @@
-import React, {useEffect} from 'react'
+import React, {Fragment, useEffect} from 'react'
+import {Link} from 'react-router-dom'
 import {getCurrentProfile} from '../actions/profile'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
+import Spinner from './layout/Spinner'
 
-const Dashboard = ({getCurrentProfile, auth, profile}) => {
+const Dashboard = ({getCurrentProfile, auth: {user}, profile:{profile, loading}}) => {
   useEffect(() => {
     getCurrentProfile()
-  }, []);
+  }, [getCurrentProfile]);
   return (
     <div className="container">
-        <h1>Dashboard</h1>
+      {loading && profile === null ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <h1 className="large text-primary">Dashboard</h1>
+          <p className="lead">
+            <i className="fa fa-user"></i>
+            Welcome {user && user.name}
+          </p>
+          {profile !== null ?
+            (<Fragment>
+              has
+            </Fragment>) : (
+              <Fragment>
+                <p>You haven't yet profile</p>
+                <Link to='/create-profile' className="btn btn-primary my-1">Create Profile</Link>
+              </Fragment>
+            )
+          }
+        </Fragment>
+      )}
     </div>
   )
 }
